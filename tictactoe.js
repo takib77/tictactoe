e5237c92722f2ff830e3c492c6786982fb3267b3
Leo <takib77@gmail.com>
2020-11-23 17:58:21 +0100

    JS just started

---
+++ tictactoe.js
@@ -0,0 +1,15 @@
+'use strict';
+
+(function xx() {
+    const sectorSelector = document.querySelectorAll('td.sector');
+        sectorSelector.addEventListener('click', () => {
+            sectorSelector.textContent = 'X';
+        })
+        console.log(sectorSelector);
+})();
+
+
+
+
+
+
